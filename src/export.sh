8b4f16520e1b7085cb757bb9289a4a89941e0473
ChipCE <login721@gmail.com>
2025-12-12 14:20:57 +0900

    default page

--- src/export.sh
+++ src/export.sh
@@ -2,13 +2,11 @@
 currentPath=${PWD}
 cd "$currentPath"
 
-rm -rf ../assets
-rm -rf ../fonts
+rm -rf ../js
 rm -rf ../img
 rm -rf ../index.html
 rm -rf ../favicon.png
 rm -rf ../data
 
 npm run build
-cp -r ./data ./dist/
 cp -r ./dist/* ../
